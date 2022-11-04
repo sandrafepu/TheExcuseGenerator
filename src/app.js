272d2a974caf383167adf43d525e7450854c9522
sandrafepu <sandrafepu@gmail.com>
2022-11-04 13:38:35 +0000

    TheExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+};
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yellet at my", "stole my", "bit my"];
+  let possetion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let pronIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let possIndx = Math.floor(Math.random() * possetion.length);
+  let wheIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    possetion[possIndx] +
+    " " +
+    where[wheIndx]
+  );
 };
